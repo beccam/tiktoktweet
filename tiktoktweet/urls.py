6733eb74d0c12a9f6eb89117ad38fbd80c03f4e1
Rebecca Mills <beccamills86@gmail.com>
2014-12-26 13:14:51 -0330

    changed application name

--- tiktoktweet/urls.py
+++ tiktoktweet/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tiktoktweet.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^tiktokadmin/', include('tiktokadmin.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
